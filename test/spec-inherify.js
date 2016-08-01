b874a2594f6a89d8c8c86698fa52cc480b61553a
Rubén López Gómez <loslopez1@icloud.com>
2016-08-01 15:34:20 +0200

    fix: inherit multilevel prototype

--- test/spec-inherify.js
+++ test/spec-inherify.js
@@ -8,10 +8,12 @@ var Inherify = require("../lib/inherify");
 
 describe('Inherify', function() {
     describe('Base constructor', function() {
-        var proto, stic, cotor, inst, args;
+        var proto, base, stic, cotor, inst, args;
 
-        beforeEach(function(){
-            proto = stic = cotor = inst = args = null;
+        (new (Inherify({'hola':1}).create({hola: 2}).create({'hola': 3}).create({'hola':4})))
+        
+        beforeEach(function() {
+            proto = stic = cotor = inst = args = base =null;
         })
 
         it('should create a base contructor', function() {
@@ -22,30 +24,35 @@ describe('Inherify', function() {
 
         it('should create a base contructor with a prototype', function() {
             cotor = Inherify({
-                foo: chai.spy(function(){
-
-                }),
+                foo: function() {
+                    this.wow = true;
+                },
                 bar: 1
             });
             inst = new cotor();
-            //console.dir(inst.__proto__);
-            console.log(inst.hasOwnProperty(inst.foo));
-            console.log(inst.bar);
-            inst.foo('bar')
+
+
             expect(inst).to.be.instanceof(cotor);
             expect(inst.__proto__).to.have.ownProperty('foo');
             expect(inst.__proto__).to.have.ownProperty('bar');
-            // expect(inst.foo).to.be.a('function');
-            // expect(inst.bar).to.be.a('number');
-            // expect(inst.foo).to.have.been.called();
-            // expect(inst.foo).to.have.been.called.with('bar');
-            // expect(inst.bar).to.be.equal(proto);
+            expect(inst.foo).to.be.a('function');
+            expect(inst.bar).to.be.a('number');
+            expect(inst.bar).to.be.equal(1);
+            expect(inst.wow).to.not.exist;
+            inst.foo = chai.spy(inst.foo);
+            inst.foo('bar');
+            expect(inst.wow).to.exist;
+            expect(inst.wow).to.be.a('boolean');
+            expect(inst.foo).to.have.been.called();
+            expect(inst.foo).to.have.been.called.with('bar');
         });
 
         it('should call the constructor function when instantiante', function() {
 
             cotor = Inherify(proto = {
                 __constructor: chai.spy(function(foo, bar) {
+                    expect(foo).to.be.equal('foo');
+                    expect(bar).to.be.equal('bar');
                 })
             });
             inst = new cotor('foo', 'bar');
@@ -68,7 +75,21 @@ describe('Inherify', function() {
     });
 
     describe('Inherital constructor', function() {
-        // this.timeout(15000);
-        // it('should print help to stdout', function(done) {});
+
+        it('should have a super constructor', function() {
+
+            base = chai.spy(require('events'));
+
+            cotor = Inherify(base, proto = {
+                __constructor: function() {
+                    expect(this._super).to.be.exist;
+                    this._super('foo');
+                }
+            });
+            inst = new cotor();
+
+            expect(base).to.have.been.called();
+            expect(base).to.have.been.called.with('foo');
+        });
     });
 });
